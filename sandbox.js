f22aa260cd6473ef5522ee23022fc978a55da14e
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-10 21:25:03 +0530

    first commit

---
+++ sandbox.js
@@ -0,0 +1,43 @@
+/*
+
+ //Chapter-1
+  Install typeScript
+  "npm install typescript"
+
+*/
+/*
+Chapter-2
+
+ //compiling Typescrit
+  //cmd
+     //tsc <filename> note:- In this case the name of file should be same as Javascript
+     //tsc <typescript filename> <javascript Filename>\
+
+const char="karan"
+ console.log(char)
+ 
+ const input=document.querySelectorAll('input')
+ console.log(input)
+
+ input.forEach(input=>{
+    console.log(input)
+ })
+
+ */
+//chapter-3
+//Types Basics
+var namee = "mario";
+var age = 30;
+var isblackbelt = false;
+//it will Show error
+//ts have strict type so it dont change dynamicly
+//namee=20;
+namee = 'luigi';
+//age='yoshi';
+age = 40;
+//isblackbelt='yes';
+isblackbelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
---
+++ sandbox.ts
@@ -0,0 +1,60 @@
+/*
+
+ //Chapter-1
+  Install typeScript
+  "npm install typescript"
+
+*/ 
+
+/*
+Chapter-2
+
+ //compiling Typescrit
+  //cmd
+     //tsc <filename> note:- In this case the name of file should be same as Javascript
+     //tsc <typescript filename> <javascript Filename>\
+
+const char="karan"
+ console.log(char)
+ 
+ const input=document.querySelectorAll('input')
+ console.log(input)
+
+ input.forEach(input=>{
+    console.log(input)
+ })
+
+ */
+
+ /*
+ //chapter-3
+   //Types Basics
+    
+let namee="mario"
+let age=30;
+let isblackbelt=false;
+
+//it will Show error
+//ts have strict type so it dont change dynamicly
+
+//namee=20;
+namee='luigi';
+
+//age='yoshi';
+age=40;
+
+//isblackbelt='yes';
+isblackbelt=true;
+
+var circ= function (diameter:number){
+   return diameter*Math.PI
+};
+
+console.log(circ(10));
+
+*/
+
+
+
+//Chapter-4
+   
